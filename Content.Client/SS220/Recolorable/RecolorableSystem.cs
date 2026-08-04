be7a85a5ee23bfd8b121db2be473e5cab3d1e17e
nerobemso <57330724+nerobemso@users.noreply.github.com>
2026-08-04 16:04:07 +0300

    trying to change layer colors

---
+++ Content.Client/SS220/Recolorable/RecolorableSystem.cs
@@ -0,0 +1,35 @@
+﻿using Robust.Client.GameObjects;
+
+namespace Content.Shared.SS220.Recolorable;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class RecolorableSystem : EntitySystem
+{
+
+    [Dependency] private SpriteSystem _spriteSystem = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<RecolorableComponent, ComponentInit>(OnInitialize);
+    }
+
+    private void OnInitialize(Entity<RecolorableComponent> ent, ref ComponentInit args)
+    {
+        if (!TryComp<SpriteComponent>(ent.Owner, out var sprite))
+            return;
+
+        ent.Comp.ColorableLayers.Add(0);
+
+        for (int i = 0; i < ent.Comp.ColorableLayers.Count; i++)
+        {
+            var layer = ent.Comp.ColorableLayers[i];
+            _spriteSystem.LayerSetColor((ent, sprite), layer, ent.Comp.Color);
+        }
+    }
+
+}
---
+++ Content.Shared/SS220/Recolorable/RecolorableComponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.SS220.Recolorable;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class RecolorableComponent : Component
+{
+    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    public List<int> ColorableLayers = new();
+
+    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    public Color Color = Color.Red;
+}
